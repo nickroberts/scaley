650a75d48889ab8d7ec14afab818ef6ae31f8be2
Nick Roberts <djparadyme@gmail.com>
2016-04-06 14:28:37 -0400

    Change index to main. Do not open dev tools. Cleanup some code to use ES6.

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
+'use strict';
+
+const HID = require('node-hid');
+const VENDOR_ID = 3471;
+const DEVICE_ID = 512;
+
 angular.module('app', [])
   .controller('AppController', function($scope, $timeout) {
-    var _this = this;
-
-    var HID = require('node-hid');
-    var VENDOR_ID = 3471;
-    var DEVICE_ID = 512;
+    let _this = this;
 
     _this.enableScale = enableScale;
     _this.getWeight = getWeight;
@@ -17,7 +19,7 @@ angular.module('app', [])
           _this.device = new HID.HID(VENDOR_ID, DEVICE_ID);
           setupScaleListener();
         } else {
-          throw 'No usb devices';
+          throw 'No usb devices detected.';
         }
       } catch (e) {
         scaleErrorHandler(e);
@@ -26,7 +28,7 @@ angular.module('app', [])
 
     function setupScaleListener() {
       _this.device.on('data', function(data) {
-        var ounces = data.readInt16LE(4)/10;
+        let ounces = data.readInt16LE(4)/10;
         if (_this.weight !== ounces) {
           _this.weight = ounces;
           $scope.$applyAsync();
@@ -42,7 +44,7 @@ angular.module('app', [])
     function activate() {
       enableScale();
     }
-    
+
     function scaleErrorHandler(e) {
       console.error(e);
       try {
